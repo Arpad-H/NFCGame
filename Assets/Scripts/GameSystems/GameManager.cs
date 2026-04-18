65687f0a829743b2d56f141d146a73f0705f9158
Arpad-H <horvaarpad@gmail.com>
2026-04-18 23:13:59 +0200

    added turn indicator

--- Assets/Scripts/GameSystems/GameManager.cs
+++ Assets/Scripts/GameSystems/GameManager.cs
@@ -5,25 +5,33 @@ using Random = System.Random;
 
 
 public class GameManager : MonoBehaviour
-    {
-        Board board;
-        PlayerSide currentPlayersTurn;
-        private void Awake()
-        {
-            board = new  Board();
-            board.SetUpBoard();
-            currentPlayersTurn = new Random().Next(0, 2) == 0 ? PlayerSide.Left : PlayerSide.Right;
-            WebSocketServerBehaviour.Instance.UpdateGameManagerReference(this);
-        }
+{
+    Board board;
+    PlayerSide currentPlayersTurn;
 
-        public void HandlePlayerPlayCard(int cardId)
-        {
-            if(board.PlaceCard(currentPlayersTurn,cardId)) NextTurn();
-            Debug.Log("invalid play, try again");
-        }
-        private void NextTurn()
-        {
-            currentPlayersTurn = currentPlayersTurn == PlayerSide.Left ? PlayerSide.Right : PlayerSide.Left;
-        }
-        
+    private void Awake()
+    {
+        board = new Board();
+        board.SetUpBoard();
+        currentPlayersTurn = new Random().Next(0, 2) == 0 ? PlayerSide.Left : PlayerSide.Right;
+        WebSocketServerBehaviour.Instance.UpdateGameManagerReference(this);
+      
     }
+
+    private void Start()
+    {
+        UIManager.Instance.SwitchPlayerTurn(currentPlayersTurn);
+    }
+
+    public void HandlePlayerPlayCard(int cardId)
+    {
+        if (board.PlaceCard(currentPlayersTurn, cardId)) NextTurn();
+        Debug.Log("invalid play, try again");
+    }
+
+    private void NextTurn()
+    {
+        currentPlayersTurn = currentPlayersTurn == PlayerSide.Left ? PlayerSide.Right : PlayerSide.Left;
+        UIManager.Instance.SwitchPlayerTurn(currentPlayersTurn);
+    }
+}
