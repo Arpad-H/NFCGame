da58ef5e157906de4d17916ff082d1470d922075
Arpad-H <horvaarpad@gmail.com>
2026-06-20 15:49:48 +0200

    added camera shake

---
+++ Assets/CameraShakeFX/Scripts/TraumaInducer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+/* Example script to apply trauma to the camera or any game object */
+public class TraumaInducer : MonoBehaviour
+{
+
+    [Tooltip("Maximum stress the effect can inflict upon objects Range([0,1])")]
+   
+    public float baseStress = 0.6f;
+    private StressReceiver receiver;
+
+    private void Start()
+    {
+        receiver = FindAnyObjectByType<StressReceiver>();
+    }
+
+    private void ShakeCamera()
+    {
+        if (!receiver) return;
+      
+        receiver.InduceStress(baseStress);
+    }
+    //test on spacebar pressed 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ShakeCamera();
+        }
+    }
+}
